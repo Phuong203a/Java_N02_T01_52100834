2f923f80af8785e788a6de6c032ae7d70e65b4d7
Phuong203a <118909698+Phuong203a@users.noreply.github.com>
2023-11-16 13:42:01 +0700

    Add files via upload

---
+++ 52100834_Lab8/models/models/User.java
@@ -0,0 +1,15 @@
+package models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private String name;
+    private int age;
+}
